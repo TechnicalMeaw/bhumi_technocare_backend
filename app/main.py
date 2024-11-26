4da5ffb5b46c7a0895a3f60a15fee97cd1434cd0
technicalmeaw <technicalmeaw@gmail.com>
2024-11-27 01:30:47 +0530

    added billing APIs router to the main app

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from app.src import models
 from app.src.database import engine
-from app.src.routers import users, auth, engineers, service, notice, attendance
+from app.src.routers import users, auth, engineers, service, notice, attendance, billing
 
 
 # models.Base.metadata.create_all(bind=engine)
@@ -15,3 +15,5 @@ app.include_router(engineers.router)
 app.include_router(service.router)
 app.include_router(notice.router)
 app.include_router(attendance.router)
+app.include_router(billing.router)
+
